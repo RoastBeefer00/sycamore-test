89dec566cf0c1a461c2d9e3aba56b5538d58a88f
Jake Jasmin <roastbeefer000@gmail.com>
2023-05-26 07:42:22 -0600

    push to pages

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,6 @@ fn App<G: Html>(cx: Scope) -> View<G> {
     let app_state = provide_context(cx, app_state);
     let search = create_signal(cx, String::new());
 
-    let handle_keyup = move |event: KeyboardEvent| {
-        if event.key() == "Enter" {
-            app_state.add_from_search(&search.get().as_ref().clone());
-            search.modify().clear();
-        }
-    };
 
     // Add recipes to database
     let json = serde_json::from_str(RECIPES);
@@ -43,11 +37,21 @@ fn App<G: Html>(cx: Scope) -> View<G> {
            .map(|recipe| recipe.add_id())
            .collect());
 
+    let handle_keyup = |event: KeyboardEvent| {
+        if event.key() == "Enter" {
+            app_state.add_from_search(search.get().as_ref().clone());
+            search.set("".to_string());
+        }
+    };
+    let remove_all_recipes = move |_| app_state.remove_all_recipes();
     let recipes_empty = create_selector(cx, || app_state.recipes.get().is_empty());
     view! { cx,
         Nav {}
         div(class="flex mx-auto my-3 lg:w-2/3 w-4/5") {
             input(class="flex-initial mx-6 w-full shadow rounded appearance-none border-8 border-indigo-700 p-2", type="search", placeholder="Press 'Enter' to search...", bind:value=search, on:keyup=handle_keyup)
+        }
+        button(class="rounded bg-red-300 text-white p-2", on:click=remove_all_recipes) {
+            "Remove All"
         }
         ((!*recipes_empty.get()).then(|| view! { cx,
                 Show_Recipes {}
--- src/recipes.rs
+++ src/recipes.rs
@@ -12,8 +12,9 @@ pub struct AppState {
 pub fn Show_Recipes<G: Html>(cx: Scope) -> View<G> {
     let app_state = use_context::<AppState>(cx);
 
-    let recipes = create_memo(cx, || app_state.recipes.get().as_ref().clone());
-
+    let recipes = create_memo(cx, || app_state.recipes.get().iter().filter(|_| true).cloned().collect::<Vec<_>>());
+    // let recipes = create_signal(cx, app_state.recipes.get().as_ref().clone());
+    // let recipes = create_selector(cx, || app_state.recipes.get());
     view! { cx,
         Keyed(
             iterable=recipes,
@@ -34,16 +35,20 @@ impl AppState {
         self.recipes.modify().clear()
     }
 
-    pub fn add_from_search(&self, search: &String) {
-        let to_add: Vec<Recipe> = self.db
+    pub fn add_from_search(&self, search: String) {
+        let mut to_add: Vec<Recipe> = self.db
             .get()
             .as_ref()
             .clone()
             .into_iter()
             .filter(|recipe| recipe.name.to_lowercase().contains(search.to_lowercase().as_str()))
             .collect();
-        self.recipes.set([self.recipes.get().as_ref().clone(), to_add].concat());
-        self.recipes.modify().sort_by_key(|recipe| recipe.id);
-        self.recipes.modify().dedup_by_key(|recipe| recipe.id)
+        /* for recipe in to_add.iter() {
+            self.recipes.modify().push(recipe.clone());
+        } */
+        to_add = [self.recipes.get().as_ref().clone(), to_add].concat();
+        to_add.sort_by_key(|recipe| recipe.id);
+        to_add.dedup_by_key(|recipe| recipe.id);
+        self.recipes.set(to_add)
     }
 }
