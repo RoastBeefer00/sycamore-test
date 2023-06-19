64409c2f95e579b2f78e09bd41556b9e6d6a88b8
Jake Jasmin <roastbeefer000@gmail.com>
2023-06-18 20:01:51 -0600

    push to pages

--- dist/sickamore-dd5a6c0634f824a2.js
+++ dist/sickamore-dd5a6c0634f824a2.js
@@ -585,7 +585,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('sickamore-f46bfce335c09e35_bg.wasm', import.meta.url);
+        input = new URL('sickamore-dd5a6c0634f824a2_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
